b8338c93c7d52da7573375a2afb43c410ee2aa36
SerhiiDior <svdior@gmail.com>
2020-01-12 19:04:24 +0200

    Update test_data.py

--- Data/test_data.py
+++ Data/test_data.py
@@ -23,7 +23,7 @@ class CreateEventData():
 
 
     TITLE = random.choice( ['New Year', 'Christmas', 'Malanka'] )
-    IMAGE = os.path.join( current_path,  'imageAddEvent\\party.jpg' )
+    IMAGE = os.path.join( current_path,  'imageAddEvent/party.jpg' )
     LOGIN_USER = 'user@gmail.com'
     PASSWORD_USER = '1qaz1qaz'
     DESCRIPTION = {"New Year": "Happy 2020 Year!!!Weclome to the Party"}
